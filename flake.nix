d85cd195ce95db0ea548ef84a90cebcb93aa3c00
paolino <paolo.veronelli@gmail.com>
2026-07-05 22:04:05 +0100

    feat: wire cardano-tx-tools closure (CHaP, crypto libs, phase-1 re-export)

--- flake.nix
+++ flake.nix
@@ -7,25 +7,41 @@
       [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
   };
   inputs = {
-    haskellNix.url = "github:input-output-hk/haskell.nix";
+    haskellNix.url =
+      "github:input-output-hk/haskell.nix/8b447d7f57d62fab9249f79bb916bc891e29b9d0";
     nixpkgs = { follows = "haskellNix/nixpkgs-unstable"; };
     flake-utils.url = "github:hamishmack/flake-utils/hkm/nested-hydraJobs";
     mkdocs.url = "github:paolino/dev-assets?dir=mkdocs";
+    iohkNix = {
+      url = "github:input-output-hk/iohk-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    CHaP = {
+      url = "github:intersectmbo/cardano-haskell-packages?ref=repo";
+      flake = false;
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, haskellNix, mkdocs, ... }:
+  outputs =
+    inputs@{ self, nixpkgs, flake-utils, haskellNix, iohkNix, CHaP, mkdocs, ... }:
     let
       version = self.dirtyShortRev or self.shortRev;
 
       perSystem = system:
         let
           pkgs = import nixpkgs {
-            overlays = [ haskellNix.overlay ];
+            overlays = [
+              iohkNix.overlays.crypto
+              haskellNix.overlay
+              iohkNix.overlays.haskell-nix-crypto
+              iohkNix.overlays.cardano-lib
+              (_final: prev: { lzma = prev.xz; })
+            ];
             inherit system;
           };
           project = import ./nix/project.nix {
             indexState = "2026-04-17T00:00:00Z";
-            inherit pkgs;
+            inherit CHaP pkgs system;
             mkdocs = mkdocs.packages.${system};
           };
           docker-image =
